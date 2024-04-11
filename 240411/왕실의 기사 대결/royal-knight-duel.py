43e9124e6a018589db8614006dda48e6816b823e
Carnival77 <52997401+carnival77@users.noreply.github.com>
2024-04-11 21:35:46 +0900

    Title: 왕실의 기사 대결, Time: 156ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240411/왕실의 기사 대결/royal-knight-duel.py
+++ 240411/왕실의 기사 대결/royal-knight-duel.py
@@ -1,125 +1,96 @@
 import sys
-from collections import deque
 input=sys.stdin.readline
+from collections import deque
 
 n,m,K=map(int,input().split())
-a=[list(map(int,input().split())) for _ in range(n)] # 0:빈칸, 1:함정, 2:벽
-b=[[0]*n for _ in range(n)]
+a=[list(map(int,input().split())) for _ in range(n)] # 체스판
+b=[[0]*n for _ in range(n)] # 디버깅용 기사맵
 
 #  상,우,하,좌
 dx=[-1,0,1,0]
 dy=[0,1,0,-1]
 
-r=[0]*m
-c=[0]*m
-nr=[0]*m
-nc=[0]*m
-h=[0]*m
-w=[0]*m
-k=[0]*m
-init_k=[0]*m
-dmg=[0]*m
-moved=[False]*m
+units=dict()
+init_k=[0]*(m+1)
 
-for no in range(m):
-    data=list(map(int,input().split()))
-    data[0]-=1
-    data[1]-=1
-    r[no]=data[0]
-    c[no]=data[1]
-    h[no]=data[2]
-    w[no]=data[3]
-    k[no]=data[4]
-    init_k[no]=data[4]
+for no in range(1,m+1):
+    x,y,h,w,k=map(int,input().split())
+    x-=1
+    y-=1
+    units[no]=[x,y,h,w,k]
+    init_k[no]=k
+    for i in range(x, x + h):
+        for j in range(y, y + w):
+            b[i][j] = no
 
-    for x in range(r[no],r[no]+h[no]):
-        for y in range(c[no],c[no]+w[no]):
-            b[x][y]=no+1
+def try_movement(start,d):
 
-def tryMove(now,d):
-
-    # 이동 관련 값 현재 값 또는 0으로 초기화
-    for i in range(m):
-        nr[i]=r[i]
-        nc[i]=c[i]
-        dmg[i]=0
-        moved[i]=False
-
-    q=deque() # 위치를 옮길 기사 후보 큐
-    q.append(now)
-    moved[now]=True
+    q=deque()
+    q.append(start)
+    dmg = [0] * (m + 1)
+    move=set() # 움직일 기사 후보
+    move.add(start)
 
     while q:
-        me=q.popleft()
-        nr[me]+=dx[d]
-        nc[me]+=dy[d]
+        now=q.popleft()
+        x,y,h,w=units[now][:4]
+        nx,ny=x+dx[d],y+dy[d]
 
-        # 이동할 곳이 범위 내인지 체크
-        if not (0<=nr[me] and nr[me]+h[me]<=n and 0<=nc[me] and nc[me]+w[me]<=n):
-            return False
+        # 기사가 이동하려는 방향의 끝에 벽이 있다면 모든 기사는 이동할 수 없게 됩니다
+        # 범위 체크
+        if not (nx>=0 and nx+h-1<n and ny>=0 and ny+w-1<n): return
+        # 벽과 함정 체크
+        for i in range(nx,nx+h):
+            for j in range(ny,ny+w):
+                if a[i][j]==2:
+                    return
+                # 각 기사들은 해당 기사가 이동한 곳에서 w×h 직사각형 내에 놓여 있는 함정의 수만큼만 피해를 입게 됩니다.
+                if a[i][j]==1:
+                    dmg[now]+=1
 
-        # 이동할 곳에 벽이나 함정이 있는지 체크
-        for x in range(nr[me],nr[me]+h[me]):
-            for y in range(nc[me],nc[me]+w[me]):
-                if a[x][y]==2:
-                    return False
-                if a[x][y]==1:
-                    dmg[me]+=1
+        # 연쇄적으로 다른 기사를 밀치는 지 체크하고, 밀치는 기사는 큐와 움직일 기사 후보에 삽입
+        for other in units.keys():
+            # 이미 움직인 기사 제외
+            if other in move:
+                continue
+            ox,oy,oh,ow=units[other][:4]
 
-        # 이번 기사가 위치를 옮긴 결과 이번 기사의 영역이 다른 기사의 영역과 중복될 경우,
-        # 다른 기사도 위치를 옮길 후보가 되어 큐에 추가된다.
-        for other in range(m):
-            # 만약 이미 움직였거나 사라진 기사는 제외
-            if moved[other] or k[other]<=0:continue
-            # 이번 기사의 이동 결과 다른 기사와 영역이 중복되는 지 아래 4가지 경우를 통해 체크한다.
-            # 아래 4가지 경우는 4가지 방향으로 이동 시 영역이 중복되지 않는 경우의 수이다.
-            # 4가지 경우 모두 True 여야 이동 시 겹치지 않는다.
-            # 이번 기사가
-            # 1) 아래로 이동 시, 위치를 옮긴 기사의 맨 아랫 칸(a)보다 다른 기사의 맨 윗 칸(b)이 더 아래
-            #    즉, a이 b보다 더 작은 숫자로 더 위에 위치하는 지 체크
-            # 2) 위로 이동 시, 위치를 옮긴 기사의 맨 윗 칸(a)보다 다른 기사의 맨 아랫 칸(b)이 더 위
-            #    즉, a이 b보다 더 큰 숫자로 더 아래 위치하는 지 체크
-            # 3) 오른쪽으로 이동 시, 위치를 옮긴 기사의 맨 오른쪽 칸(a)보다 다른 기사의 맨 왼쪽 칸(b)이 더 오른쪽
-            #    즉, a이 b보다 더 작은 숫자로 더 왼쪽에 위치하는 지 체크
-            # 4) 왼쪽으로 이동 시, 위치를 옮긴 기사의 맨 왼쪽 칸(a)보다 다른 기사의 맨 오른쪽 칸(b)이 더 왼쪽
-            #    즉, a이 b보다 더 큰 숫자로 더 오른쪽에 위치하는 지 체크
+            # 겹치지 않는 경우
+            if nx>ox+oh-1 or ny>oy+ow-1 or nx+h-1<ox or ny+w-1<oy:
+                continue
+            move.add(other)
+            q.append(other)
 
-            # 겹치는 경우 - 아래와 반대
-            if r[other] <= nr[me] + h[me] - 1 and nr[me]<=r[other]+h[other]-1 and c[other]<=nc[me]+w[me]-1 and nc[me]<=c[other]+w[other]-1:
-                moved[other]=True
-                q.append(other)
+    # 명령을 받은 기사는 피해를 입지 않으며
+    dmg[start]=0
 
-            # # 겹치지 않는 경우
-            # if r[other] > nr[me] + h[me] - 1 or nr[me]>r[other]+h[other]-1 or c[other]>nc[me]+w[me]-1 or nc[me]>c[other]+w[other]-1:
-            #     pass
-            # else:
-            #     moved[other]=True
-            #     q.append(other)
-
-    dmg[now]=0
-
-    return True
-round=0
-for round in range(1,K+1):
-    now,d=map(int,input().split())
-    now-=1
-    if k[now]<=0:
-        continue
-    if tryMove(now,d):
-        for i in range(m):
-            r[i]=nr[i]
-            c[i]=nc[i]
-            k[i]-=dmg[i]
+    # 움직일 기사 후보 내 기사들을 움직이고 정보 업데이트
+    for no in move:
+        x,y,h,w,k=units[no]
+        # 각 기사마다 피해를 받은 만큼 체력이 깎이게 되며, 현재 체력 이상의 대미지를 받을 경우 기사는 체스판에서 사라지게 됩니다
+        # 기사들은 모두 밀린 이후에 대미지를 입게 됩니다.
+        if k<=dmg[no]:
+            units.pop(no)
+        else:
+            nx,ny=x+dx[d],y+dy[d]
+            units[no]=[nx,ny,h,w,k-dmg[no]]
 
     b = [[0] * n for _ in range(n)]
-    for no in range(m):
-        for x in range(r[no],r[no]+h[no]):
-            for y in range(c[no],c[no]+w[no]):
-                b[x][y]=no+1
+    for no in range(1,m+1):
+        if no in units.keys():
+            x,y,h,w,k=units[no]
+            for i in range(x,x+h):
+                for j in range(y,y+w):
+                    b[i][j]=no+1
+
+for round in (1,K+1):
+    no,d=map(int,input().split())
+
+    # 체스판에서 사라진 기사에게 명령을 내리면 아무런 반응이 없게 됩니다.
+    if no in units.keys():
+        try_movement(no,d)
 
 ans=0
-for no in range(m):
-    if k[no]<=0:continue
-    ans+=(init_k[no]-k[no])
-
+for no in units:
+    ans+=init_k[no]-units[no][4]
 print(ans)
