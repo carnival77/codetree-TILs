f505912616033056ba84aaabb26296530fe6db5b
Carnival77 <52997401+carnival77@users.noreply.github.com>
2024-04-11 17:22:46 +0900

    Title: 왕실의 기사 대결, Time: 246ms, Memory: 27MB, Status: Passed - CodeTree

--- 240411/왕실의 기사 대결/royal-knight-duel.py
+++ 240411/왕실의 기사 대결/royal-knight-duel.py
@@ -1,102 +1,119 @@
+import sys
 from collections import deque
+input=sys.stdin.readline
 
-# 전역 변수들을 정의합니다.
-MAX_N = 31
-MAX_L = 41
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+n,m,K=map(int,input().split())
+a=[list(map(int,input().split())) for _ in range(n)] # 0:빈칸, 1:함정, 2:벽
+b=[[0]*n for _ in range(n)]
 
-info = [[0 for _ in range(MAX_L)] for _ in range(MAX_L)]
-bef_k = [0 for _ in range(MAX_N)]
-r = [0 for _ in range(MAX_N)]
-c = [0 for _ in range(MAX_N)]
-h = [0 for _ in range(MAX_N)]
-w = [0 for _ in range(MAX_N)]
-k = [0 for _ in range(MAX_N)]
-nr = [0 for _ in range(MAX_N)]
-nc = [0 for _ in range(MAX_N)]
-dmg = [0 for _ in range(MAX_N)]
-is_moved = [False for _ in range(MAX_N)]
+#  상,우,하,좌
+dx=[-1,0,1,0]
+dy=[0,1,0,-1]
 
+r=[0]*m
+c=[0]*m
+nr=[0]*m
+nc=[0]*m
+h=[0]*m
+w=[0]*m
+k=[0]*m
+init_k=[0]*m
+dmg=[0]*m
+moved=[False]*m
 
-# 움직임을 시도해봅니다.
-def try_movement(idx, dir):
-    q = deque()
-    is_pos = True
+for no in range(m):
+    data=list(map(int,input().split()))
+    data[0]-=1
+    data[1]-=1
+    r[no]=data[0]
+    c[no]=data[1]
+    h[no]=data[2]
+    w[no]=data[3]
+    k[no]=data[4]
+    init_k[no]=data[4]
 
-    # 초기화 작업입니다.
-    for i in range(1, n + 1):
-        dmg[i] = 0
-        is_moved[i] = False
-        nr[i] = r[i]
-        nc[i] = c[i]
+    for x in range(r[no],r[no]+h[no]):
+        for y in range(c[no],c[no]+w[no]):
+            b[x][y]=no+1
 
-    q.append(idx)
-    is_moved[idx] = True
+def tryMove(now,d):
+
+    # 이동 관련 값 현재 값 또는 0으로 초기화
+    for i in range(m):
+        nr[i]=r[i]
+        nc[i]=c[i]
+        dmg[i]=0
+        moved[i]=False
+
+    q=deque() # 위치를 옮길 기사 후보 큐
+    q.append(now)
+    moved[now]=True
 
     while q:
-        x = q.popleft()
+        me=q.popleft()
+        nr[me]+=dx[d]
+        nc[me]+=dy[d]
 
-        nr[x] += dx[dir]
-        nc[x] += dy[dir]
-
-        # 경계를 벗어나는지 체크합니다.
-        if nr[x] < 1 or nc[x] < 1 or nr[x] + h[x] - 1 > l or nc[x] + w[x] - 1 > l:
+        # 이동할 곳이 범위 내인지 체크
+        if not (0<=nr[me] and nr[me]+h[me]<=n and 0<=nc[me] and nc[me]+w[me]<=n):
             return False
 
-        # 대상 조각이 다른 조각이나 장애물과 충돌하는지 검사합니다.
-        for i in range(nr[x], nr[x] + h[x]):
-            for j in range(nc[x], nc[x] + w[x]):
-                if info[i][j] == 1:
-                    dmg[x] += 1
-                if info[i][j] == 2:
+        # 이동할 곳에 벽이나 함정이 있는지 체크
+        for x in range(nr[me],nr[me]+h[me]):
+            for y in range(nc[me],nc[me]+w[me]):
+                if a[x][y]==2:
                     return False
+                if a[x][y]==1:
+                    dmg[me]+=1
 
-        # 다른 조각과 충돌하는 경우, 해당 조각도 같이 이동합니다.
-        for i in range(1, n + 1):
-            if is_moved[i] or k[i] <= 0:
-                continue
-            if r[i] > nr[x] + h[x] - 1 and nr[x] > r[i] + h[i] - 1 and c[i] > nc[x] + w[x] - 1 and nc[x] > c[i] + w[i] - 1:
-                continue
-            # if nr[x] > r[i] + h[i] - 1:
-            #     continue
-            # if c[i] > nc[x] + w[x] - 1:
-            #     continue
-            # if nc[x] > c[i] + w[i] - 1:
-            #     continue
+        # 이번 기사가 위치를 옮긴 결과 이번 기사의 영역이 다른 기사의 영역과 중복될 경우,
+        # 다른 기사도 위치를 옮길 후보가 되어 큐에 추가된다.
+        for other in range(m):
+            # 만약 이미 움직였거나 사라진 기사는 제외
+            if moved[other] or k[other]<=0:continue
+            # 이번 기사의 이동 결과 다른 기사와 영역이 중복되는 지 아래 4가지 경우를 통해 체크한다.
+            # 아래 4가지 경우는 4가지 방향으로 이동 시 영역이 중복되지 않는 경우의 수이다.
+            # 4가지 경우 모두 True 여야 이동 시 겹치지 않는다.
+            # 이번 기사가
+            # 1) 아래로 이동 시, 위치를 옮긴 기사의 맨 아랫 칸(a)보다 다른 기사의 맨 윗 칸(b)이 더 아래
+            #    즉, a이 b보다 더 작은 숫자로 더 위에 위치하는 지 체크
+            # 2) 위로 이동 시, 위치를 옮긴 기사의 맨 윗 칸(a)보다 다른 기사의 맨 아랫 칸(b)이 더 위
+            #    즉, a이 b보다 더 큰 숫자로 더 아래 위치하는 지 체크
+            # 3) 오른쪽으로 이동 시, 위치를 옮긴 기사의 맨 오른쪽 칸(a)보다 다른 기사의 맨 왼쪽 칸(b)이 더 오른쪽
+            #    즉, a이 b보다 더 작은 숫자로 더 왼쪽에 위치하는 지 체크
+            # 4) 왼쪽으로 이동 시, 위치를 옮긴 기사의 맨 왼쪽 칸(a)보다 다른 기사의 맨 오른쪽 칸(b)이 더 왼쪽
+            #    즉, a이 b보다 더 큰 숫자로 더 오른쪽에 위치하는 지 체크
 
-            is_moved[i] = True
-            q.append(i)
+            if r[other] > nr[me] + h[me] - 1 or nr[me]>r[other]+h[other]-1 or c[other]>nc[me]+w[me]-1 or nc[me]>c[other]+w[other]-1:
+                pass
+            else:
+                moved[other]=True
+                q.append(other)
 
-    dmg[idx] = 0
+    dmg[now]=0
+
     return True
+round=0
+for round in range(1,K+1):
+    now,d=map(int,input().split())
+    now-=1
+    if k[now]<=0:
+        continue
+    if tryMove(now,d):
+        for i in range(m):
+            r[i]=nr[i]
+            c[i]=nc[i]
+            k[i]-=dmg[i]
 
+    b = [[0] * n for _ in range(n)]
+    for no in range(m):
+        for x in range(r[no],r[no]+h[no]):
+            for y in range(c[no],c[no]+w[no]):
+                b[x][y]=no+1
 
-# 특정 조각을 지정된 방향으로 이동시키는 함수입니다.
-def move_piece(idx, move_dir):
-    if k[idx] <= 0:
-        return
+ans=0
+for no in range(m):
+    if k[no]<=0:continue
+    ans+=(init_k[no]-k[no])
 
-    # 이동이 가능한 경우, 실제 위치와 체력을 업데이트합니다.
-    if try_movement(idx, move_dir):
-        for i in range(1, n + 1):
-            r[i] = nr[i]
-            c[i] = nc[i]
-            k[i] -= dmg[i]
-
-
-# 입력값을 받습니다.
-l, n, q = map(int, input().split())
-for i in range(1, l + 1):
-    info[i][1:] = map(int, input().split())
-for i in range(1, n + 1):
-    r[i], c[i], h[i], w[i], k[i] = map(int, input().split())
-    bef_k[i] = k[i]
-
-for _ in range(q):
-    idx, d = map(int, input().split())
-    move_piece(idx, d)
-
-# 결과를 계산하고 출력합니다.
-ans = sum([bef_k[i] - k[i] for i in range(1, n + 1) if k[i] > 0])
 print(ans)
