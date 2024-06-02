5ad89785ad7edb55b4602c8dad1dc4afbfb699dc
Carnival77 <52997401+carnival77@users.noreply.github.com>
2024-06-02 11:19:36 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 923ms, Memory: 29MB, Status: Passed - CodeTree

--- 240602/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
+++ 240602/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
@@ -1,53 +1,55 @@
-# 시간 복잡도
-# 도형은 19가지 모양이 나올 수 있고, N * M 격자의 각 칸에 도형의 시작점이 놓일 수 있으므로,
-# O(19 * (N*M)^2) < 1억
+n,m=map(int,input().split())
 
-# 솔루션
-# 뫼 산 모양 하나를 제외한 나머지 모양은, 임의의 칸에서 시작하여, 연속하는 3개의 인접 칸을 방문한 것으로 나타낼 수 있다.
-# 따라서 뫼 산 모양 하나를 제외한 나머지는 재귀 함수로 나타낼 수 있다.
+a=[list(map(int,input().split())) for _ in range(n)]
+ans=0
 
+#  우,하,좌,상
+dx=[0,1,0,-1]
+dy=[1,0,-1,0]
 
-dx = [0,0,1,-1]
-dy = [1,-1,0,0]
-n,m = map(int,input().split())
-a = [list(map(int,input().split())) for _ in range(n)]
-c = [[False]*m for _ in range(n)]
-def go(x,y,sum,cnt):
-    if cnt == 4:
-        global ans
-        if ans < sum:
-            ans = sum
+visit=[[0]*m for _ in range(n)]
+
+def inBoard(nx,ny):
+    if 0<=nx<n and 0<=ny<m:
+        return True
+    return False
+
+def dfs(x,y,cnt,s):
+    global ans,visit
+
+    if cnt==4:
+        ans=max(ans,s)
         return
-    if x < 0 or x >= n or y < 0 or y >= m:
+    if not inBoard(x,y):
         return
-    if c[x][y]:
+    if visit[x][y]==1:
         return
-    c[x][y] = True
+    visit[x][y]=1
     for k in range(4):
-        go(x+dx[k],y+dy[k],sum+a[x][y],cnt+1)
-    c[x][y] = False
-ans = 0
-for i in range(n):
-    for j in range(m):
-        go(i,j,0,0)
-        if j+2 < m:
-            temp = a[i][j] + a[i][j+1] + a[i][j+2]
-            if i-1 >= 0:
-                temp2 = temp + a[i-1][j+1]
-                if ans < temp2:
-                    ans = temp2
-            if i+1 < n:
-                temp2 = temp + a[i+1][j+1]
-                if ans < temp2:
-                    ans = temp2
-        if i+2 < n:
-            temp = a[i][j] + a[i+1][j] + a[i+2][j]
-            if j+1 < m:
-                temp2 = temp + a[i+1][j+1]
-                if ans < temp2:
-                    ans = temp2
-            if j-1 >= 0:
-                temp2 = temp + a[i+1][j-1]
-                if ans < temp2:
-                    ans = temp2
+        nx,ny=x+dx[k],y+dy[k]
+        dfs(nx,ny,cnt+1,s+a[x][y])
+    visit[x][y]=0
+
+def simulate(x,y,s):
+    global ans
+
+    if inBoard(x-1,y) and inBoard(x+1,y):
+        ns=s+a[x-1][y]+a[x+1][y]
+        if inBoard(x,y+1):
+            ans=max(ans,ns+a[x][y+1])
+        if inBoard(x,y-1):
+            ans=max(ans,ns+a[x][y-1])
+
+    if inBoard(x,y-1) and inBoard(x,y+1):
+        ns=s+a[x][y-1]+a[x][y+1]
+        if inBoard(x+1,y):
+            ans=max(ans,ns+a[x+1][y])
+        if inBoard(x-1,y):
+            ans=max(ans,ns+a[x-1][y])
+
+for x in range(n):
+    for y in range(m):
+        dfs(x,y,0,0)
+        simulate(x,y,a[x][y])
+
 print(ans)
