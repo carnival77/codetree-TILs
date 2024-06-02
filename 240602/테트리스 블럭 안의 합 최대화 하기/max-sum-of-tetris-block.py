107b64344fd17c6dfbc0b80f08fafd66892624f3
Carnival77 <52997401+carnival77@users.noreply.github.com>
2024-06-02 10:42:37 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 8041ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240602/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
+++ 240602/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
@@ -21,16 +21,34 @@ def dfs(sx,sy,cnt,s,visit):
 
     for k in range(4):
         nx,ny=sx+dx[k],sy+dy[k]
-        if not inBoard(nx,ny) or visit[nx][ny]:continue
-        visit[nx][ny]=True
+        if not inBoard(nx,ny) or visit[nx][ny]==1:continue
+        visit[nx][ny]=1
         dfs(nx,ny,cnt+1,s+a[nx][ny],visit)
-        visit[nx][ny]=False
+        visit[nx][ny]=0
+
+def simulate(x,y,s):
+    global ans
+
+    if inBoard(x-1,y) and inBoard(x+1,y):
+        ns=s+a[x-1][y]+a[x+1][y]
+        if inBoard(x,y+1):
+            ans=max(ans,ns+a[x][y+1])
+        if inBoard(x,y-1):
+            ans=max(ans,ns+a[x][y-1])
+
+    if inBoard(x,y-1) and inBoard(x,y+1):
+        ns=s+a[x][y-1]+a[x][y+1]
+        if inBoard(x+1,y):
+            ans=max(ans,ns+a[x+1][y])
+        if inBoard(x-1,y):
+            ans=max(ans,ns+a[x-1][y])
 
 for x in range(n):
     for y in range(m):
-        visit=[[False]*m for _ in range(n)]
-        visit[x][y]=True
+        visit=[[0]*m for _ in range(n)]
+        visit[x][y]=1
         dfs(x,y,1,a[x][y],visit)
-        visit[x][y]=False
+        visit[x][y]=0
+        simulate(x,y,a[x][y])
 
 print(ans)
