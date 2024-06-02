11bc94f83f474d83c48f92150f687be3c2a294be
Carnival77 <52997401+carnival77@users.noreply.github.com>
2024-06-02 11:23:47 +0900

    Title: 테트리스 블럭 안의 합 최대화 하기, Time: 8095ms, Memory: 35MB, Status: Time Limit Exceed - CodeTree

--- 240602/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
+++ 240602/테트리스 블럭 안의 합 최대화 하기/max-sum-of-tetris-block.py
@@ -7,28 +7,24 @@ ans=0
 dx=[0,1,0,-1]
 dy=[1,0,-1,0]
 
-visit=[[0]*m for _ in range(n)]
-
 def inBoard(nx,ny):
     if 0<=nx<n and 0<=ny<m:
         return True
     return False
 
-def dfs(x,y,cnt,s):
-    global ans,visit
+def dfs(sx,sy,cnt,s,visit):
+    global ans
 
     if cnt==4:
         ans=max(ans,s)
         return
-    if not inBoard(x,y):
-        return
-    if visit[x][y]==1:
-        return
-    visit[x][y]=1
+
     for k in range(4):
-        nx,ny=x+dx[k],y+dy[k]
-        dfs(nx,ny,cnt+1,s+a[x][y])
-    visit[x][y]=0
+        nx,ny=sx+dx[k],sy+dy[k]
+        if not inBoard(nx,ny) or visit[nx][ny]==1:continue
+        visit[nx][ny]=1
+        dfs(nx,ny,cnt+1,s+a[nx][ny],visit)
+        visit[nx][ny]=0
 
 def simulate(x,y,s):
     global ans
@@ -47,9 +43,31 @@ def simulate(x,y,s):
         if inBoard(x-1,y):
             ans=max(ans,ns+a[x-1][y])
 
-for x in range(n):
-    for y in range(m):
-        dfs(x,y,0,0)
-        simulate(x,y,a[x][y])
+for i in range(n):
+    for j in range(m):
+        visit=[[0]*m for _ in range(n)]
+        visit[i][j]=1
+        dfs(i,j,1,a[i][j],visit)
+        visit[i][j]=0
+        if j + 2 < m:
+            temp = a[i][j] + a[i][j + 1] + a[i][j + 2]
+            if i - 1 >= 0:
+                temp2 = temp + a[i - 1][j + 1]
+                if ans < temp2:
+                    ans = temp2
+            if i + 1 < n:
+                temp2 = temp + a[i + 1][j + 1]
+                if ans < temp2:
+                    ans = temp2
+        if i + 2 < n:
+            temp = a[i][j] + a[i + 1][j] + a[i + 2][j]
+            if j + 1 < m:
+                temp2 = temp + a[i + 1][j + 1]
+                if ans < temp2:
+                    ans = temp2
+            if j - 1 >= 0:
+                temp2 = temp + a[i + 1][j - 1]
+                if ans < temp2:
+                    ans = temp2
 
 print(ans)
