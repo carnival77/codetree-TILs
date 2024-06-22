1e493273a4df5be16fd1d46cdc1d90a2b6d3f1f7
Carnival77 <52997401+carnival77@users.noreply.github.com>
2024-06-22 15:07:46 +0900

    Title: 병원 거리 최소화하기, Time: 201ms, Memory: 26MB, Status: Passed - CodeTree

--- 240622/병원 거리 최소화하기/min-of-hospital-distance.py
+++ 240622/병원 거리 최소화하기/min-of-hospital-distance.py
@@ -1,63 +1,33 @@
+import sys
 from itertools import combinations
+input=sys.stdin.readline
 
-n,m = map(int,input().split())
+MAX=int(1e9)
 
-board=[]
+n,m=map(int,input().split())
 
-for _ in range(n):
-    board.append(list(map(int,input().split())))
+a=[list(map(int,input().split())) for _ in range(n)]
 
-houses=[]
-chickens=[]
+ans=MAX
 
-for i in range(n):
-    for j in range(n):
-        if board[i][j] == 1:
-            houses.append((i+1,j+1))
-        elif board[i][j] == 2:
-            chickens.append((i+1,j+1))
+house=[]
+chicken=[]
 
-# 치킨집들 중에 m개 고르는 모든 경우의 수
-chicken_comb = list(combinations(chickens,m))
+for x in range(n):
+    for y in range(n):
+        if a[x][y]==1:
+            house.append([x,y])
+        elif a[x][y]==2:
+            chicken.append([x,y])
 
-# 도시의 치킨 거리 = 모든 집의 치킨 거리 합
-# 각 집의 치킨 거리 = min(각 집 위치 부터 m개의 치킨집 중 각 치킨집까지의 거리)
-def get_sum(m_chicken):
-    sum=0
-    for house in houses:
-        x,y = house
-        each_cd = 1000
-        for chicken in m_chicken:
-            a,b=chicken
-            dist = abs(x - a) + abs(y - b)
-            each_cd = min(each_cd, dist)
-        sum += each_cd
-    return sum
+for comb in combinations(chicken,m):
+    res=0
+    for hx,hy in house:
+        min_d=MAX
+        for cx,cy in comb:
+            d=abs(hx-cx)+abs(hy-cy)
+            min_d=min(d,min_d)
+        res+=min_d
+    ans=min(ans,res)
 
-answer=1000
-
-# m개 치킨집들의 각 경우
-for m_chicken in chicken_comb:
-    # 도시의 치킨 거리의 최솟값
-    answer = min(answer,get_sum(m_chicken))
-
-print(answer)
-
-# print(chicken_comb)
-# sum=0
-#
-# for house in houses:
-#     x,y = house
-#     each_cd = 1000
-#     for m_chicken in chicken_comb:
-#         print("m_chicken : ",m_chicken)
-#         for chicken in m_chicken:
-#             print("chicken : " ,chicken)
-#             a,b = chicken
-#             dist = abs(x-a) + abs(y-b)
-#             print("dist :",dist)
-#             each_cd = min(each_cd,dist)
-#             print("each_cd : ",each_cd)
-#     sum += each_cd
-#
-# print(sum)
+print(ans)
