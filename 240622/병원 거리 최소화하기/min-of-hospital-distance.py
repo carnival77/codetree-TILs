bb5750185694d00d7a93c8a97c0bcb8129553f24
Carnival77 <52997401+carnival77@users.noreply.github.com>
2024-06-22 15:08:25 +0900

    Title: 병원 거리 최소화하기, Time: 220ms, Memory: 26MB, Status: Passed - CodeTree

--- 240622/병원 거리 최소화하기/min-of-hospital-distance.py
+++ 240622/병원 거리 최소화하기/min-of-hospital-distance.py
@@ -1,33 +1,80 @@
-import sys
+from collections import deque
 from itertools import combinations
-input=sys.stdin.readline
 
 MAX=int(1e9)
 
 n,m=map(int,input().split())
-
-a=[list(map(int,input().split())) for _ in range(n)]
-
+a=[list(map(int,input().split())) for _ in range(n)] # 0 : 빈칸, 1 : 사람, 2 : 병원
+# b=[[0]*n for _ in range(n)]
 ans=MAX
 
-house=[]
-chicken=[]
+# dx=[-1,0,1,0]
+# dy=[0,-1,0,1]
 
+# 사람,병원 위치 찾기
+p,h=[],[]
 for x in range(n):
     for y in range(n):
-        if a[x][y]==1:
-            house.append([x,y])
-        elif a[x][y]==2:
-            chicken.append([x,y])
+        if a[x][y]==2:
+            h.append([x+1,y+1])
+        elif a[x][y]==1:
+            p.append([x+1,y+1])
+            # b[x][y]=1
 
-for comb in combinations(chicken,m):
-    res=0
-    for hx,hy in house:
-        min_d=MAX
-        for cx,cy in comb:
-            d=abs(hx-cx)+abs(hy-cy)
-            min_d=min(d,min_d)
-        res+=min_d
-    ans=min(ans,res)
+# def inBoard(nx,ny):
+#     if 0<=nx<n and 0<=ny<n:
+#         return True
+#     return False
+
+# def bfs(sx,sy):
+#     q=deque()
+#     q.append((sx,sy))
+#     d=[[-1]*n for _ in range(n)]
+#     d[sx][sy]=0
+#     cand=[]
+#
+#     while q:
+#         x,y=q.popleft()
+#         for k in range(4):
+#             nx,ny=x+dx[k],y+dy[k]
+#             if not inBoard(nx,ny) or d[nx][ny]!=-1:
+#                 continue
+#             q.append((nx,ny))
+#             d[nx][ny]=d[x][y]+1
+#             if b[nx][ny]==2:
+#                 return d[nx][ny]
+#                 # cand.append(d[nx][ny])
+#     # cand.sort()
+#     # return cand[0]
+
+# 병원들 중 m개 뽑는 경우의 수
+
+
+
+# for comb in combinations(h,m):
+#     res=0
+#     # 병원 설치
+#     # for x,y in comb:
+#     #     b[x][y]=2
+#     # 각 사람 위치에서 bfs 로 제일 가까운 병원까지의 거리 탐색
+#     for x,y in p:
+#         # dist=bfs(x,y)
+#         # res+=dist
+#         res=getDist(x,y)
+#     # 병원 해체
+#     # for x,y in comb:
+#     #     b[x][y]=0
+#     # 최솟값 갱신
+#     ans=min(ans,res)
+
+for comb in combinations(h,m):
+    s=0
+    for px,py in p:
+        dist = MAX
+        for hx,hy in comb:
+            d=abs(px-hx)+abs(py-hy)
+            dist=min(dist,d)
+        s+=dist
+    ans=min(ans,s)
 
 print(ans)
